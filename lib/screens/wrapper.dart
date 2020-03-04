023769860d75d9ebdbd7182d82edc3a719ae79ca
Murilo Beltrame Correia <murilo.correia@raizen.com>
2020-03-03 23:22:21 -0300

    add firebase libs and base screen structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:brewcrew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //return Home or Authenticate
+
+    return Home();
+  }
+}
