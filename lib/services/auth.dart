66a59d96874ce1feb9a8eac80b4de628f6354ccd
Murilo Beltrame Correia <murilo.correia@raizen.com>
2020-03-03 23:38:19 -0300

    auth service

---
+++ lib/services/auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign anom
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign with email
+
+  // register
+
+  // sigout 
+
+}
