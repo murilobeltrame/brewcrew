42971d83504f05547ffa729ab6ef864f405bc1f5
Murilo Beltrame Correia <murilo.correia@raizen.com>
2020-03-06 21:31:37 -0300

    loading settings

--- lib/screens/home/settings.dart
+++ lib/screens/home/settings.dart
@@ -1,5 +1,9 @@
+import 'package:brewcrew/modules/user.dart';
+import 'package:brewcrew/services/database.dart';
+import 'package:brewcrew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brewcrew/shared/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -17,53 +21,68 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Update your brew settings',
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20,),
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            items: sugars.map((sugar) => DropdownMenuItem(
-              value: sugar,
-              child: Text('$sugar sugars'),
-            )).toList(),
-            onChanged: (val) => setState(() => _currentSugars = val),
-            value: _currentSugars ?? '0'
-          ),
-          SizedBox(height: 20,),
-          Slider(
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            divisions: 8,
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            max: 900,
-            min: 100,
-            onChanged: (val) => setState(() => _currentStrength = val.round()),
-            value: (_currentStrength ?? 100).toDouble()
-          ),
-          SizedBox(height: 20,),
-          RaisedButton(
-            color: Colors.pink[400],
-            child: Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () async {
 
-            },
-          )
-        ],
-      ),
+    final user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+
+        if (snapshot.hasData) {
+          UserData userData = snapshot.data;
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Update your brew settings',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: textInputDecoration,
+                  initialValue: userData.name,
+                  validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (val) => setState(() => _currentName = val),
+                ),
+                SizedBox(height: 20,),
+                DropdownButtonFormField(
+                  decoration: textInputDecoration,
+                  items: sugars.map((sugar) => DropdownMenuItem(
+                    value: sugar,
+                    child: Text('$sugar sugars'),
+                  )).toList(),
+                  onChanged: (val) => setState(() => _currentSugars = val),
+                  value: _currentSugars ?? userData.sugars
+                ),
+                SizedBox(height: 20,),
+                Slider(
+                  activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                  divisions: 8,
+                  inactiveColor: Colors.brown[_currentStrength ?? userData.strength],
+                  max: 900,
+                  min: 100,
+                  onChanged: (val) => setState(() => _currentStrength = val.round()),
+                  value: (_currentStrength ?? userData.strength).toDouble()
+                ),
+                SizedBox(height: 20,),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+
+                  },
+                )
+              ],
+            ),
+          );
+        } else {
+          return Loading();
+        }        
+      },
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brewcrew/modules/brew.dart';
+import 'package:brewcrew/modules/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -18,12 +19,26 @@ class DatabaseService {
     }).toList();
   }
 
+  UserData _userDataFromSnapShot(DocumentSnapshot snapshot){
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'] ?? '',
+      sugars: snapshot.data['sugars'] ?? '0',
+      strength: snapshot.data['strength'] ?? 0
+    );
+  }
+
   // Collection Reference
   final CollectionReference _brewCollection = Firestore.instance.collection('brews');
 
   // Get Brews Stream
   Stream<List<Brew>> get brews {
     return _brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+  //brew for user
+  Stream<UserData> get userData{
+    return _brewCollection.document(uid).snapshots().map(_userDataFromSnapShot);
   }
 
   // 
